fc5c4fa52a52ed6c1eb8eea557ec2ec318d0bb4f
8oDolaz1 <gzagranichnov@mail.ru>
2025-10-14 17:50:40 +0300

    Import error fixed

--- embeddings_squeeze/__init__.py
+++ embeddings_squeeze/__init__.py
@@ -5,14 +5,28 @@ embeddings_squeeze: Vector Quantization for Segmentation Model Compression
 __version__ = "0.1.0"
 __author__ = "Your Name"
 
-from .models.vq.quantizer import VectorQuantizer
+from .models.quantizers import (
+    VQWithProjection,
+    FSQWithProjection,
+    LFQWithProjection,
+    ResidualVQWithProjection,
+    BaseQuantizer,
+)
 from .models.backbones.base import SegmentationBackbone
 from .models.lightning_module import VQSqueezeModule
+from .models.baseline_module import BaselineSegmentationModule
 from .data.base import BaseDataModule
+from .loggers import setup_clearml
 
 __all__ = [
-    "VectorQuantizer",
+    "VQWithProjection",
+    "FSQWithProjection",
+    "LFQWithProjection",
+    "ResidualVQWithProjection",
+    "BaseQuantizer",
     "SegmentationBackbone", 
     "VQSqueezeModule",
+    "BaselineSegmentationModule",
     "BaseDataModule",
+    "setup_clearml",
 ]
