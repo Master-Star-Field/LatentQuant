08fad7eae66d8576f7f78bf639768bfb93d68e79
8oDolaz1 <gzagranichnov@mail.ru>
2025-10-15 12:18:03 +0300

    Num workers update

--- embeddings_squeeze/data/oxford_pet.py
+++ embeddings_squeeze/data/oxford_pet.py
@@ -38,7 +38,7 @@ class OxfordPetDataModule(BaseDataModule):
         self,
         data_path: str = './data',
         batch_size: int = 4,
-        num_workers: int = 6,
+        num_workers: int = 4,
         pin_memory: bool = True,
         image_size: int = 224,
         subset_size: int = None,
