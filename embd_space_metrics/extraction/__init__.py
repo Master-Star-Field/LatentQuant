7ad08486ee2d11a43bb7fe036a297d1fc716ce58
Alim Albogachiev <alimalbogachiev@yandex.ru>
2025-10-16 17:16:43 +0300

    m

--- embd_space_metrics/extraction/__init__.py
+++ embd_space_metrics/extraction/__init__.py
@@ -1,11 +1,13 @@
 """Feature extraction utilities."""
 
-from .features import extract_features_from_backbone
-from .model_loader import load_checkpoint, load_original_backbone
+from .features import extract_features_from_backbone, extract_quantized_features
+from .model_loader import load_checkpoint, load_original_backbone, find_checkpoints
 
 __all__ = [
     "extract_features_from_backbone",
+    "extract_quantized_features",
     "load_checkpoint",
     "load_original_backbone",
+    "find_checkpoints",
 ]
 
